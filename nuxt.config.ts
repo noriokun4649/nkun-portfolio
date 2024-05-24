b318374c0dfe4414258cc1d63cb24812c3c84f62
noriokun4649 <noriokun.official.mail@gmail.com>
2024-05-25 06:23:15 +0900

    CSS Purge Add.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxtjs/google-fonts", "@nuxt/image", "@nuxtjs/robots"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxtjs/google-fonts", "@nuxt/image", "@nuxtjs/robots", "nuxt-purgecss"],
   css: ["~/assets/css/main.css"],
   app: {
     head: {
